7b1810db6b491ddbcade4543922b611c3fec7164
mrtsrktt <mrtsrktt@users.noreply.github.com>
2026-04-16 23:21:48 +0300

    feat: add Google Ads conversion tracking tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,6 +56,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="tr">
       <body>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18058299425"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-18058299425');
+            `
+          }}
+        />
         <Providers>
           {children}
           <Toaster position="top-right" />
