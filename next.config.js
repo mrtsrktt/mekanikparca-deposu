660d181844e1ffdf833dbedc49e05797f035b6ca
mrtsrktt <mrtsrktt@users.noreply.github.com>
2026-04-16 22:37:37 +0300

    fix: add 301 redirect for old HP-5c product URL

--- next.config.js
+++ next.config.js
@@ -10,6 +10,15 @@ const nextConfig = {
       bodySizeLimit: '10mb',
     },
   },
+  async redirects() {
+    return [
+      {
+        source: '/urun/heat-transfer-fluid-hp-5c-10-litre-isi-transfer-sivisi-don-koruma',
+        destination: '/urun/isi-pompasi-isi-transfer-sivisi-hp-5c-10-litre-fernox-heat-transfer-fluid-don-koruma',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
